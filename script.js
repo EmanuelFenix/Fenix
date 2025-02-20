6281949f3e6321fad94fd1c341d000bbe284de9e
Emanuel da Silva <emanuelsilvapro1500@gmail.com>
2025-02-20 23:37:42 +0100

    TIC TAC TOE
    Código fonte - Emanuel Domingos da Silva

---
+++ script.js
@@ -0,0 +1,223 @@
+//
+
+const win = () => {
+    let matriz = document.querySelectorAll('.ar')
+
+    if (matriz[0].innerHTML == matriz[1].innerHTML && matriz[1].innerHTML == matriz[2].innerHTML && matriz[0].innerHTML != '' && matriz[1].innerHTML != '' && matriz[2].innerHTML != '') {
+        matriz.forEach((e) => {
+            e.style.pointerEvents = 'none'
+            e.style.opacity = '.30'
+        })
+        matriz[0].style.backgroundColor = 'gold'
+        matriz[0].style.transform = 'scale(1.2)'
+        matriz[0].style.zIndex = '9999'
+        matriz[0].style.opacity = '1'
+        matriz[1].style.backgroundColor = 'gold'
+        matriz[1].style.transform = 'scale(1.2)'
+        matriz[1].style.zIndex = '9999'
+        matriz[1].style.opacity = '1'
+        matriz[2].style.backgroundColor = 'gold'
+        matriz[2].style.transform = 'scale(1.2)'
+        matriz[2].style.zIndex = '9999'
+        matriz[2].style.opacity = '1'
+    } else if (matriz[3].innerHTML == matriz[4].innerHTML && matriz[4].innerHTML == matriz[5].innerHTML && matriz[3].innerHTML != '' && matriz[4].innerHTML != '' && matriz[5].innerHTML != '') {
+        matriz.forEach((e) => {
+            e.style.pointerEvents = 'none'
+            e.style.opacity = '.30'
+        })
+        matriz[3].style.backgroundColor = 'gold'
+        matriz[3].style.transform = 'scale(1.2)'
+        matriz[3].style.zIndex = '9999'
+        matriz[3].style.opacity = '1'
+        matriz[4].style.backgroundColor = 'gold'
+        matriz[4].style.transform = 'scale(1.2)'
+        matriz[4].style.zIndex = '9999'
+        matriz[4].style.opacity = '1'
+        matriz[5].style.backgroundColor = 'gold'
+        matriz[5].style.transform = 'scale(1.2)'
+        matriz[5].style.zIndex = '9999'
+        matriz[5].style.opacity = '1'
+    } else if (matriz[6].innerHTML == matriz[7].innerHTML && matriz[7].innerHTML == matriz[8].innerHTML && matriz[6].innerHTML != '' && matriz[7].innerHTML != '' && matriz[8].innerHTML != '') {
+        matriz.forEach((e) => {
+            e.style.pointerEvents = 'none'
+            e.style.opacity = '.30'
+        })
+        matriz[6].style.backgroundColor = 'gold'
+        matriz[6].style.transform = 'scale(1.2)'
+        matriz[6].style.zIndex = '9999'
+        matriz[6].style.opacity = '1'
+        matriz[7].style.backgroundColor = 'gold'
+        matriz[7].style.transform = 'scale(1.2)'
+        matriz[7].style.zIndex = '9999'
+        matriz[7].style.opacity = '1'
+        matriz[8].style.backgroundColor = 'gold'
+        matriz[8].style.transform = 'scale(1.2)'
+        matriz[8].style.zIndex = '9999'
+        matriz[8].style.opacity = '1'
+    } else if (matriz[0].innerHTML == matriz[4].innerHTML && matriz[4].innerHTML == matriz[8].innerHTML && matriz[0].innerHTML != '' && matriz[4].innerHTML != '' && matriz[8].innerHTML != '') {
+        matriz.forEach((e) => {
+            e.style.pointerEvents = 'none'
+            e.style.opacity = '.30'
+        })
+        matriz[0].style.backgroundColor = 'gold'
+        matriz[0].style.transform = 'scale(1.2)'
+        matriz[0].style.zIndex = '9999'
+        matriz[0].style.opacity = '1'
+        matriz[4].style.backgroundColor = 'gold'
+        matriz[4].style.transform = 'scale(1.2)'
+        matriz[4].style.zIndex = '9999'
+        matriz[4].style.opacity = '1'
+        matriz[8].style.backgroundColor = 'gold'
+        matriz[8].style.transform = 'scale(1.2)'
+        matriz[8].style.zIndex = '9999'
+        matriz[8].style.opacity = '1'
+    } else if (matriz[0].innerHTML == matriz[3].innerHTML && matriz[3].innerHTML == matriz[6].innerHTML && matriz[0].innerHTML != '' && matriz[3].innerHTML != '' && matriz[6].innerHTML != '') {
+        matriz.forEach((e) => {
+            e.style.pointerEvents = 'none'
+            e.style.opacity = '.30'
+        })
+        matriz[0].style.backgroundColor = 'gold'
+        matriz[0].style.transform = 'scale(1.2)'
+        matriz[0].style.zIndex = '9999'
+        matriz[0].style.opacity = '1'
+        matriz[3].style.backgroundColor = 'gold'
+        matriz[3].style.transform = 'scale(1.2)'
+        matriz[3].style.zIndex = '9999'
+        matriz[3].style.opacity = '1'
+        matriz[6].style.backgroundColor = 'gold'
+        matriz[6].style.transform = 'scale(1.2)'
+        matriz[6].style.zIndex = '9999'
+        matriz[6].style.opacity = '1'
+    } else if (matriz[1].innerHTML == matriz[4].innerHTML && matriz[4].innerHTML == matriz[7].innerHTML && matriz[1].innerHTML != '' && matriz[4].innerHTML != '' && matriz[7].innerHTML != '') {
+        matriz.forEach((e) => {
+            e.style.pointerEvents = 'none'
+            e.style.opacity = '.30'
+        })
+        matriz[1].style.backgroundColor = 'gold'
+        matriz[1].style.transform = 'scale(1.2)'
+        matriz[1].style.zIndex = '9999'
+        matriz[1].style.opacity = '1'
+        matriz[4].style.backgroundColor = 'gold'
+        matriz[4].style.transform = 'scale(1.2)'
+        matriz[4].style.zIndex = '9999'
+        matriz[4].style.opacity = '1'
+        matriz[7].style.backgroundColor = 'gold'
+        matriz[7].style.transform = 'scale(1.2)'
+        matriz[7].style.zIndex = '9999'
+        matriz[7].style.opacity = '1'
+    } else if (matriz[2].innerHTML == matriz[4].innerHTML && matriz[4].innerHTML == matriz[6].innerHTML && matriz[2].innerHTML != '' && matriz[4].innerHTML != '' && matriz[6].innerHTML != '') {
+        matriz.forEach((e) => {
+            e.style.pointerEvents = 'none'
+            e.style.opacity = '.30'
+        })
+        matriz[2].style.backgroundColor = 'gold'
+        matriz[2].style.transform = 'scale(1.2)'
+        matriz[2].style.zIndex = '9999'
+        matriz[2].style.opacity = '1'
+        matriz[4].style.backgroundColor = 'gold'
+        matriz[4].style.transform = 'scale(1.2)'
+        matriz[4].style.zIndex = '9999'
+        matriz[4].style.opacity = '1'
+        matriz[6].style.backgroundColor = 'gold'
+        matriz[6].style.transform = 'scale(1.2)'
+        matriz[6].style.zIndex = '9999'
+        matriz[6].style.opacity = '1'
+    } else if (matriz[2].innerHTML == matriz[5].innerHTML && matriz[5].innerHTML == matriz[8].innerHTML && matriz[2].innerHTML != '' && matriz[5].innerHTML != '' && matriz[8].innerHTML != '') {
+        matriz.forEach((e) => {
+            e.style.pointerEvents = 'none'
+            e.style.opacity = '.30'
+        })
+        matriz[2].style.backgroundColor = 'gold'
+        matriz[2].style.transform = 'scale(1.2)'
+        matriz[2].style.zIndex = '9999'
+        matriz[2].style.opacity = '1'
+        matriz[5].style.backgroundColor = 'gold'
+        matriz[5].style.transform = 'scale(1.2)'
+        matriz[5].style.zIndex = '9999'
+        matriz[5].style.opacity = '1'
+        matriz[8].style.backgroundColor = 'gold'
+        matriz[8].style.transform = 'scale(1.2)'
+        matriz[8].style.zIndex = '9999'
+        matriz[8].style.opacity = '1'
+    } else {
+        let i = 0
+
+        matriz.forEach((item) => {
+            if (item.innerHTML != "") {
+                i++
+            }
+        })
+
+        if (i == 9) {
+            alert("EMPATE!")
+            //location.reload()
+        }
+    }
+}
+
+//
+
+const check = ({ target }) => {
+
+    if (elemento.classList.contains('O')) {
+        target.innerHTML = `O`
+        target.style.color = '#006aff'
+        target.setAttribute('disabled', '')
+        elemento.classList.remove('O')
+    } else if (elemento.classList.contains('X')) {
+        target.innerHTML = `X`
+        target.style.color = '#ff0000'
+        target.setAttribute('disabled', '')
+        elemento.classList.add('O')
+    }
+
+    win()
+
+}
+
+const criarElemento = (tag, className) => {
+    elemento = document.createElement(tag)
+    elemento.className = className
+    elemento.addEventListener('click', check)
+
+    return elemento
+}
+
+const reload = () => {
+    location.reload()
+}
+
+const loadGame = () => {
+    const elemento1 = criarElemento('button', 'area a1 O X ar')
+    const elemento2 = criarElemento('button', 'area a1 O X ar')
+    const elemento3 = criarElemento('button', 'area a1 O X ar')
+    const elemento4 = criarElemento('button', 'area a1 O X ar')
+    const elemento5 = criarElemento('button', 'area a1 O X ar')
+    const elemento6 = criarElemento('button', 'area a1 O X ar')
+    const elemento7 = criarElemento('button', 'area a1 O X ar')
+    const elemento8 = criarElemento('button', 'area a1 O X ar')
+    const elemento9 = criarElemento('button', 'area a1 O X ar')
+
+
+    mom.appendChild(elemento1)
+    mom.appendChild(elemento2)
+    mom.appendChild(elemento3)
+    mom.appendChild(elemento4)
+    mom.appendChild(elemento5)
+    mom.appendChild(elemento6)
+    mom.appendChild(elemento7)
+    mom.appendChild(elemento8)
+    mom.appendChild(elemento9)
+
+    const button = document.createElement('button')
+    button.className = 'button'
+    button.addEventListener('click', reload)
+    button.innerHTML = `Recarregar`
+    btn.appendChild(button)
+}
+
+const mom = document.querySelector('#grid')
+const back = document.querySelector('.area')
+const btn = document.querySelector('#btn')
+
+loadGame()
